a6fac50e5179ec985974188746d55b2c50f1c411
derDetki <98264867+derDetki@users.noreply.github.com>
2024-04-20 13:10:46 +0200

    verbesserung

--- FH/rekursionExample.c
+++ FH/rekursionExample.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 void printNumbers(int n){
-    if (n==1){
-        printf("%d",n);
+    if (n == 1){
+        printf("%d ", n);
         return;
     }
     printNumbers(n-1);
-    printf("%d", n);
+    printf("%d ", n);
 }
 
 int main (int argc, char const *argv[])
