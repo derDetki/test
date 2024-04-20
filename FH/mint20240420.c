69ca115754621b5bc612b51201206f5d30504e9b
derDetki <98264867+derDetki@users.noreply.github.com>
2024-04-20 13:06:19 +0200

    funktioniert

---
+++ FH/mint20240420.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void printNumbers(int n){
+    if (n==1){
+        printf("%d",n);
+        return;
+    }
+    printNumbers(n-1);
+    printf("%d", n);
+}
+
+int main (int argc, char const *argv[])
+{
+    printNumbers(10);
+    return 0;
+}
